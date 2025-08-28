f22607d70f4ef93eb987f734ebfd82878b931091
Joao-Lucas-Pontes-Freitas <joaolucaspontesfreitas@ufu.br>
2025-08-27 21:55:26 -0300

    fim trabalho 12

--- trabalho12/ex5-infinite-scroll/more-products.php
+++ trabalho12/ex5-infinite-scroll/more-products.php
@@ -1,13 +1,13 @@
 <?php
 
-class Product
+class Product // Definição classe produtos
 {
   public $id;
   public $name;
   public $price;
   public $imagePath;
 
-  function __construct($id, $name, $price, $imagePath)
+  function __construct($id, $name, $price, $imagePath) // Construtor da classe produtos
   {
     $this->id = $id;
     $this->name = $name; 
@@ -15,7 +15,7 @@ class Product
     $this->imagePath = $imagePath;
   }
 }
-
+// Array mockado de 7 produtos para ficarem sendo renderizados na página
 $products = array(
   new Product(1, 'Smart TV LED 55', 2900, 'images/tv.webp'),
   new Product(2, 'Smartphone 6.5 ABC', 1590, 'images/smartphone.webp'),
@@ -25,7 +25,8 @@ $products = array(
   new Product(6, 'Fone Headset ABC', 250, 'images/headset.webp'),
   new Product(7, 'Pen-drive de 64GB', 90, 'images/pen-drive.webp')
 );
- 
+
+// Aleatorinza os indeices para poder pegar produtos aleatórios e não ficar sempre gerando na mesma ordem
 $randProds = [
   $products[rand(0, 6)],
   $products[rand(0, 6)],
@@ -34,5 +35,5 @@ $randProds = [
   $products[rand(0, 6)]
 ];
 
-header('Content-type: application/json');
-echo json_encode($randProds);
+header('Content-type: application/json'); // Especifica que retorna json
+echo json_encode($randProds); // Transforma o body em json e retorna
